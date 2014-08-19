07a4c8fb1defecf05e8e3db33831249661a67c69
Guo Yu <i@guoyu.me>
2014-08-19 23:04:47 +0800

    update to 0.1.1

--- src/controllers/global.js
+++
@@ -1,6 +0,0 @@
-// global ctrler
-airpub.controller('global', function($scope) {
-  if (!airpubConfigs) 
-    return console.error(new Error('airpub 缺少必要的配置文件!'));
-  $scope.configs = airpubConfigs;
-});
